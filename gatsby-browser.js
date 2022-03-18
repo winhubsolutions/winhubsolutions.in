d06b59df2904421e21f9088fbd80a9c548536f94
winhubsolutions <winhubsolutions@gmail.com>
2022-03-19 03:12:19 +0530

    winhub1

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,4 +2,8 @@ import "@fontsource/dm-sans"
 import "@fontsource/dm-sans/700.css"
 import "@fontsource/dm-mono"
 import "@fontsource/dm-mono/500.css"
-import "./src/styles.css"
+
+import "./src/assets/css/style.css"
+import "./src/assets/css/wordpress.css"
+import "./src/assets/css/font-awesome.min.css"
+
